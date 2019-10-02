051bc769ee9b52c666ddcbaa0a9dfaf30ab4d9b7
Andrew Nelson <evildarkarchon@gmail.com>
2019-10-01 19:34:07 -0700

    Remove GlobalSuppressions.cs and add to .gitignore

--- GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:STO_Event_Calendar.Program.Main(System.String[])")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:STO_Event_Calendar.JSON.WriteJson(System.String)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1065:Do not raise exceptions in unexpected locations", Justification = "<Pending>", Scope = "member", Target = "~M:STO_Event_Calendar.Prompts.Equals(STO_Event_Calendar.Prompts)~System.Boolean")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:STOEventCalendar.JSON.WriteJson(System.String)")]
